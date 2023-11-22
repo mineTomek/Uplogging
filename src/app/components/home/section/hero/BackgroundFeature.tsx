7ccf7d8c1e2c834bceeeaf83f29fffc77db00d4c
mineTomek <lukom.tomek@gmail.com>
2023-11-22 17:24:21 +0100

    Add gap between parts of the background feature

--- src/app/components/home/section/hero/BackgroundFeature.tsx
+++ src/app/components/home/section/hero/BackgroundFeature.tsx
@@ -1,7 +1,7 @@
 export default function BackgroundFeature() {
   return (
     <div className='fixed bottom-0 left-0 right-0 top-0 -z-10 flex items-center justify-center'>
-      <div className='blur-3xl [animation:_spin_15s_linear_infinite]'>
+      <div className='flex gap-6 blur-3xl [animation:_spin_15s_linear_infinite]'>
         <div className='h-32 w-64 translate-x-16 rounded-full bg-gradient-to-b from-primary-600 to-primary-200' />
         <div className='h-32 w-64 translate-x-16 rotate-45 rounded-full bg-gradient-to-r from-accent-600 to-accent-200' />
       </div>
