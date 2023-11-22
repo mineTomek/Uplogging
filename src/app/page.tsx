f4132483e276ef7d2f2200b3765e93302658196b
mineTomek <lukom.tomek@gmail.com>
2023-11-22 21:29:45 +0100

    Fix the unfocusing of one of the cards

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,9 @@ export default function Home() {
             <h2 className='ml-10 text-[3vw]'>Subtitle</h2>
           </div>
         </div>
-        <div className='row-span-2 flex items-center'>
-          <div className='mx-auto aspect-[9/19.5] w-1/3 -translate-y-1/4 translate-x-full rotate-6 rounded-lg border bg-slate-50 shadow-lg dark:bg-black dark:[box-shadow:#333_0_0_50px_inset] peer'></div>
-          <div className='mx-auto aspect-[9/19.5] w-1/3 -translate-x-full translate-y-1/4 -rotate-6 rounded-lg border bg-slate-50 shadow-lg dark:bg-black dark:[box-shadow:#333_0_0_50px_inset] peer-hover:blur-sm transition-[filter] duration-1000'></div>
+        <div className='row-span-2 flex items-center [&:has(.left-card:hover)_.right-card]:unfocused-card [&:has(.right-card:hover)_.left-card]:unfocused-card'>
+          <div className='left-card mx-auto aspect-[9/19.5] w-1/3 -translate-y-1/4 translate-x-full rotate-6 rounded-lg border bg-slate-50 shadow-lg transition duration-500 dark:bg-black dark:[box-shadow:#333_0_0_50px_inset]'></div>
+          <div className='right-card mx-auto aspect-[9/19.5] w-1/3 -translate-x-full translate-y-1/4 -rotate-6 rounded-lg border bg-slate-50 shadow-lg transition duration-500 dark:bg-black dark:[box-shadow:#333_0_0_50px_inset]'></div>
         </div>
       </div>
       <div className='h-5 w-full -translate-y-4 rounded-b-xl border-b bg-white/40 dark:bg-black/40' />
