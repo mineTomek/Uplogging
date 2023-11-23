1a777329852c4b51e2774befaab4c91ec2cf6432
mineTomek <lukom.tomek@gmail.com>
2023-11-23 14:24:26 +0100

    Make the feature cards less opaque

--- src/app/components/home/section/features/Feature.tsx
+++ src/app/components/home/section/features/Feature.tsx
@@ -7,7 +7,7 @@ export default function Feature(props: {
     <div
       className={`${props.colSpan && 'col-span-2'} ${
         props.rowSpan && 'row-span-2'
-      } m-1 rounded-lg border p-2 text-left text-sm shadow-md`}
+      } m-1 rounded-lg border bg-white/40 p-2 text-left text-sm shadow-md dark:bg-black/40`}
     >
       {props.title}
     </div>
